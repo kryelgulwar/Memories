40c4c71cd94f81576eea92ba0ba727ae272d0c37
Karan Rao Yelgulwar <107773965+kryelgulwar@users.noreply.github.com>
2023-11-17 19:36:14 +0530

    m

--- server/index.js
+++ server/index.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://memo-frontend-4dikebkaj-karans-projects-f5cecbc1.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const CONNECTION_URL = 'mongodb+srv://karanyelgulwar:1nEv1EuKVVj93S4W@cluster0.wcoomx0.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
