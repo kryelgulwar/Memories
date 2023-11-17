f4d8abf2aea48a55d4f39ccd711c2079db91a4ae
Karan Rao Yelgulwar <107773965+kryelgulwar@users.noreply.github.com>
2023-11-17 18:22:38 +0530

    memo

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb://localhost:27017/memories';
+const CONNECTION_URL = 'mongodb+srv://karanyelgulwar:1nEv1EuKVVj93S4W@cluster0.wcoomx0.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
